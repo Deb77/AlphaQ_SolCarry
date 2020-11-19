23b6994f80d4262cbc79e548ed2d2906005c6da1
chickoocoutinho <benito.coutinho@gmail.com>
2020-11-19 17:39:59 +0530

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import React from 'react'
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+import Home from './index';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Router>
+        <nav>
+          NAV Here
+        </nav>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        <footer>
+          Footer Here
+        </footer>
+    </Router>
     </Provider>
     
   )
